e5df9c43614a7867518ae18e387f325717fc4f8a
Stefan Boboc <bobocstefan675@gmail.com>
2023-01-27 01:29:54 +0200

    graphics-options-added

--- Assets/OptionsMenu.cs
+++ Assets/OptionsMenu.cs
@@ -2,14 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using TMPro;
 
 public class OptionsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    public TMP_Dropdown dropdown;
+
+    void Start() 
+    {
+        dropdown.value = QualitySettings.GetQualityLevel();
+    }
 
     public void SetVolume(float volume = 1)
     {
         audioMixer.SetFloat("volume", Mathf.Log10(volume) * 20);
         Debug.Log(volume);
+    }
+
+    public void SetQuality(int value) 
+    {
+        QualitySettings.SetQualityLevel(value);
+        switch (value) 
+        {
+            case 0:
+                dropdown.image.color = new Color32(243, 167, 0, 255);
+                break;
+
+            case 1:
+                dropdown.image.color = new Color32(237, 120, 0, 255);
+                break;
+
+            case 2:
+                dropdown.image.color = new Color32(237, 70, 0, 255);
+                break;
+        }
     }
 }
